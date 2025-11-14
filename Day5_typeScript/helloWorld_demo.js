5d74f72c329af317195db9ef641169cf1e82ee13
TanishaYadav241 <tanishayadav485@gmail.com>
2025-11-14 17:05:17 +0530

    Added Day5_typeScript with all files

---
+++ Day5_typeScript/helloWorld_demo.js
@@ -0,0 +1,7 @@
+// This is a typescript file that prints "Hello, World!" to the console with the help of a function
+function greet(name) {
+    return "Hello, ".concat(name, "!");
+}
+var Message = greet("World");
+console.log(Message);
+// every declaration has its type defined explicitly ie function parameter 'name' is of type string and 
